4e1fd47257ea1972459aab3ad6cfd3e58ee2ac5c
h4mz411y <hamzabashar93@yahoo.com>
2022-06-04 18:16:20 +0300

    m

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,76 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
 
-describe('Linked List', () => {
+describe('Instantiate a Linked List', () => {
+  const linked = new LinkedList()
   it('works', () => {
-    expect(true).toBeTruthy();
+    expect(linked.head).toBeFalsy();
+  });
+});
+
+describe('Can insert to linked list', () => {
+  const linked = new LinkedList()
+  linked.insert(4)
+  it('works', () => {
+    expect(linked.head.value).toEqual(4);
+  });
+});
+
+describe('Head points always to the first element', () => {
+  const linked = new LinkedList()
+  linked.insert(4)
+  linked.insert(24)
+  linked.insert(62)
+  linked.insert(192)
+  it('works', () => {
+    expect(linked.head.value).toEqual(192);
+  });
+});
+
+describe('Can properly insert multiple nodes into the linked list', () => {
+  const linked = new LinkedList()
+  let arr = []
+  for(let i = 0; i < 50; i++) {
+    linked.insert(i)
+    arr.push(i)
+  }
+  it('works', () => {
+    arr.forEach((val) => {
+      expect(linked.includes(val)).toBeTruthy();
+    })
+  });
+});
+
+describe('Will return true when finding a value within the linked list that exists', () => {
+  const linked = new LinkedList()
+  linked.insert(4)
+  linked.insert(24)
+  linked.insert(62)
+  linked.insert(192)
+  it('works', () => {
+    expect(linked.includes(24)).toBeTruthy();
+  });
+});
+
+describe('Will return false when searching for a value in the linked list that does not exist', () => {
+  const linked = new LinkedList()
+  linked.insert(4)
+  linked.insert(24)
+  linked.insert(62)
+  linked.insert(192)
+  it('works', () => {
+    expect(linked.includes(94)).toBeFalsy();
+  });
+});
+
+describe('Can properly return a collection of all the values that exist in the linked list', () => {
+  const linked = new LinkedList()
+  linked.insert(4)
+  linked.insert(5)
+  linked.insert(100)
+  it('works', () => {
+    expect(linked.toString()).toBe("{100}=>{5}=>{4}=>NULL");
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,49 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value, pointer = null) {
+    this.value = value
+    this.pointer = pointer
+  }
 }
 
+
+class LinkedList {
+
+  constructor(head = null) {
+    this.head = head;
+  }
+
+    insert(value) {
+      let oldHead = this.head
+      this.head = new Node(value, oldHead)
+    }
+
+    includes(value) {
+      let head = this.head
+      let res = false
+      while(head) {
+        if(head.value == value) {
+          res = true
+          break;
+        }
+        head = head.pointer
+      }
+      return res
+    }
+
+    toString() {
+      let head = this.head
+      let res = ''
+      while(head) {
+        res += `{${head.value}}=>`
+        head = head.pointer
+      }
+      res += `NULL`
+      return res
+    }
+}
+
+
 module.exports = LinkedList;
+
