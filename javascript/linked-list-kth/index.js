dc6f99e8d506c713ca9a9e2df21f45bb8e35ff65
h4mz411y <hamzabashar93@yahoo.com>
2022-06-15 03:38:22 +0300

    CC07

---
+++ javascript/linked-list-kth/index.js
@@ -0,0 +1,64 @@
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+
+}
+class LinkedList {
+  constructor() {
+    this.tail = null;
+    this.head = null;
+    this.size = 0;
+  }
+
+  addFirst(data) {
+    if (this.head === null) {
+      this.head = new Node(data);
+      this.tail = new Node(data);
+    } else {
+      this.head = new Node(data, this.head);
+      this.size++
+
+    }
+  }
+
+  print() {
+    let current = this.head;
+    let sum = []
+    while (current) {
+      sum.push(`${current.data}->`)
+      current = current.next;
+    }
+    return sum
+  }
+
+  lastkth(k) {
+    if (k > this.size) {
+      return "Exception"
+    }
+    if (k < 0) {
+      return "K IN NEGATIVE"
+    }
+    else {
+      let place = this.size - k
+      let current = this.head;
+      let counter = 0
+      while (current) {
+        if (place == counter) {
+          return (current.data)
+        }
+        counter++
+        current = current.next;
+      }
+    }
+  }
+
+
+
+}
+
+
+
+module.exports = LinkedList;
