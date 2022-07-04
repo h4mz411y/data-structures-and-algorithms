7b857440b342fa142978f9dcbf24bbec47207193
h4mz411y <hamzabashar93@yahoo.com>
2022-07-04 05:31:35 +0300

    CC15

---
+++ javascript/Binary-ree-and-BST-Implementation/README.md
@@ -0,0 +1,7 @@
+# Binary-ree-and-BST-Implementation
+
+ ## [Pull Requests](https://github.com/h4mz411y/data-structures-and-algorithms/pulls?q=is%3Apr+is%3Aclosed)
+
+ ## Test
+
+![Tree](./treeTeestImage.PNG)
---
+++ javascript/Binary-ree-and-BST-Implementation/binary-tree-search.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const Node = require("./Node");
+
+class BinaryTreeSearch {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  Add(value) {
+    let current = this.root;
+    if (!current) {
+      this.root = new Node(value);
+    } else {
+      while (current) {
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = new Node(value);
+            break;
+          }
+          current = current.left;
+        } else {
+          if (!current.right) {
+            current.right = new Node(value);
+            break;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  Contains(value) {
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+module.exports = BinaryTreeSearch;
